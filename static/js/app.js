2f5a864ee711ad77448918ad7b3127839e4a1924
Trotyl Yu <yzj1995@vip.qq.com>
2016-03-11 16:18:17 +0800

    Implement Wechat popover in footer.

---
+++ static/js/app.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#wechat-link').click(function() {
+        $('#weiLayer').show();
+    });
+    $('#hide-wechat-link').click(function() {
+        $('#weiLayer').hide();
+    });
+});
