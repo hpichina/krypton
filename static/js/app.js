7739bf9f0bb8f489e87f6bbebd95c7b1b6a66d46
Trotyl Yu <yzj1995@vip.qq.com>
2016-03-23 14:18:08 +0800

    Implement product type highlight.

--- static/js/app.js
+++ static/js/app.js
@@ -9,3 +9,43 @@ $(document).ready(function() {
         $('#coop-region-list').toggle();
     });
 });
+
+function highlight(type, id) {
+    switch(type) {
+        case 'product_type':
+            var product_types = $('#product-types');
+            if(!product_types) break;
+            product_types
+                .find('li')
+                .toArray()
+                .map(function(li) { return $(li).find('a'); })
+                .forEach(function(a) {
+                    var href = a.attr('href');
+                    if(id === '') {
+                        var query = parseQuery(href);
+                        if(JSON.stringify(query) === '{}') {
+                            a.removeClass('btn-default');
+                            a.addClass('btn-warning');
+                        }
+                    }
+                    else if(a.attr('href').endsWith(id)) {
+                        a.removeClass('btn-default');
+                        a.addClass('btn-warning');
+                    }
+                });
+            break;
+    }
+}
+
+function parseQuery(query) {
+    var res = {};
+    if(query.indexOf('?') >= 0) {
+        query = query.split('?')[1];
+    }
+    var pairs = query.split('&');
+    pairs.forEach(function(pair) {
+        var datas = pair.split('=');
+        res[datas[0]] = datas[1];
+    });
+    return res;
+}
